aba6168e877dd4e966c16722a8fcfe48f4172cf6
Kirill Prilepa <89772688+thamata@users.noreply.github.com>
2023-12-05 16:35:40 +0100

    Create iot_configs.h

---
+++ Azure_IoT_Hub_ESP32/iot_configs.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "SSID"
+#define IOT_CONFIG_WIFI_PASSWORD "PASSWORD"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "iot-hub.net"
+#define IOT_CONFIG_DEVICE_ID "DEVICE_ID"
+#define IOT_CONFIG_DEVICE_KEY "DEVICE_KEY"
+
+// Publish 1 message every 10 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 10000
